3a0561967c6a11ff68d870f406b244cfda45d659
subhabaha <51040380+subhabaha@users.noreply.github.com>
2023-03-13 00:45:31 +0530

    Update Literature_Review_Writer.py

--- Literature_Review_Writer.py
+++ Literature_Review_Writer.py
@@ -1,19 +1,20 @@
 import openai
 import streamlit as st
 #from charset_normalizer import md__mypyc
+import config
 
 openai.api_key=config.api_key
 
 def main():
-    st.title("Topic Writeup and References Generator")
+    st.title("Literature Review Writer")
     notes = st.text_area("Enter Topic Information:")
-    if st.button("Generate Writeup and References"):
+    if st.button("Generate Literature Review"):
         with st.spinner("Generating Writeup and References..."):
             response = openai.Completion.create(
               model="text-davinci-003",
               prompt=f"I want you to write short literature review on below topic.\n\n{notes}\n\nDescription:",
               temperature=0.7,
-              max_tokens=256,
+              max_tokens=4000,
               top_p=1,
               frequency_penalty=0,
               presence_penalty=0
