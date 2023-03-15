f08ba0d3d5768a21740f42b6bf5089f7138bd0c7
subhabaha <51040380+subhabaha@users.noreply.github.com>
2023-03-15 08:17:02 +0530

    Update Paraphraser.py

--- Paraphraser.py
+++ Paraphraser.py
@@ -10,7 +10,7 @@ def main():
     notes = st.text_area("Enter Text to Paraphraser:")
     if st.button("Generate Writeup"):
         with st.spinner("Generating Writeup ..."):
-            response = openai.Completion.create(
+            response = openai.ChatCompletion.create(
               model="gpt-3.5-turbo-0301",
               prompt=f"paraphrase and extend the text \n\n{notes}\n\nDescription:",
               temperature=0.7,
