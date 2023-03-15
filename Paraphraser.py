e6bd8f43a9f6e1fe365d22db7db68c6b5ba19e24
subhabaha <51040380+subhabaha@users.noreply.github.com>
2023-03-15 07:06:54 +0530

    Add files via upload

---
+++ Paraphraser.py
@@ -0,0 +1,27 @@
+import openai
+import streamlit as st
+#from charset_normalizer import md__mypyc
+
+openai.api_key=config.api_key
+
+def main():
+    st.title("Paraphraser")
+    notes = st.text_area("Enter Text to Paraphraser:")
+    if st.button("Generate Writeup"):
+        with st.spinner("Generating Writeup ..."):
+            response = openai.Completion.create(
+              model="text-davinci-003",
+              prompt=f"I want you to paraphrase the text \n\n{notes}\n\nDescription:",
+              temperature=0.7,
+              max_tokens=256,
+              top_p=1,
+              frequency_penalty=0,
+              presence_penalty=0
+            )
+        description = response['choices'][0]['text']
+        st.subheader("Generated Writeup:")
+        st.write(description)
+
+
+if __name__ == '__main__':
+   main()
