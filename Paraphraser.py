3328e4ec097a04955c38e1ffe94629729d2942d4
subhabaha <51040380+subhabaha@users.noreply.github.com>
2023-03-16 22:58:19 +0530

    Update Paraphraser.py

--- Paraphraser.py
+++ Paraphraser.py
@@ -10,16 +10,16 @@ def main():
     notes = st.text_area("Enter Text to Paraphrase:")
     if st.button("Generate Writeup"):
         with st.spinner("Generating Writeup ..."):
-            response = openai.Completion.create(
-              model="text-davinci-003",
-              prompt=f"write detailed literature review with references and in-line citations on topic \n\n{notes}\n\nDescription:",
-              temperature=0.7,
-              max_tokens=3000,
-              top_p=1,
-              frequency_penalty=0,
-              presence_penalty=0
+            response = openai.ChatCompletion.create(
+              model="gpt-3.5-turbo",
+              messages=["role":"user","content"="write long literature review with references and in-line citations on topic \n\n{notes}\n\nDescription:",
+              #temperature=0.7,
+              #max_tokens=3000,
+              #top_p=1,
+              #frequency_penalty=0,
+              #presence_penalty=0
             )
-        description = response['choices'][0]['text']
+        description = response['choices'][0]['message']['content']
         st.subheader("Generated Writeup:")
         st.write(description)
 
