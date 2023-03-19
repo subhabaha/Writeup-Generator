18b60fc09b83c854884eba3389c13fc718d9e686
subhabaha <51040380+subhabaha@users.noreply.github.com>
2023-03-19 19:06:14 +0530

    Update Literature_Review_Writer.py

--- Literature_Review_Writer.py
+++ Literature_Review_Writer.py
@@ -19,7 +19,7 @@ def main():
         with st.spinner("Generating Writeup ...."):
             response = openai.ChatCompletion.create(
               model="gpt-3.5-turbo",
-              messages=[{'role':'user','content':f'You act as reseracher. Write research review of more than 2000 words and include real references and in-line citations on topic \n\n{notes}\n\nDescription:'}]
+              messages=[{'role':'user','content':f'You act as reseracher. Write research paper with more than 3000 words and include real references and in-line citations on topic \n\n{notes}\n\nDescription:'}]
               #temperature=0.7,
               #max_tokens=3000,
               #top_p=1,
