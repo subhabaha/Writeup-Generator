74e63ddb51e7cf07fcce9f4139b32cf955654fdf
subhabaha <51040380+subhabaha@users.noreply.github.com>
2023-03-19 18:53:14 +0530

    Update Literature_Review_Writer.py

--- Literature_Review_Writer.py
+++ Literature_Review_Writer.py
@@ -10,18 +10,18 @@ def main():
     st.subheader('This app has been developed by :red[Dr. Subhabaha Pal].')
     st.text('The background AI model used is ChatGPT model developed by OpenAI.') 
     st.text('Please mail to subhabaha@msn.com for suggestions and improvements.')
-    st.text('Please write any topic of interest in the below box to get detailed writeup.')
+    st.text('Please write any topic of interest in the below box to get detailed writeup. This is for reference purpose only and the researcher requested to cross-check validity of the generated content before final use.')
     notes = st.text_area("Enter Topic Information:")
     if st.button("Generate Literature Review"):
-        with st.spinner("Generating Writeup and References..."):
-            response = openai.Completion.create(
-              model="text-davinci-003",
-              prompt=f"I want you to write literature review with references and citations on below topic.\n\n{notes}\n\nDescription:",
-              temperature=0.7,
-              max_tokens=4000,
-              top_p=1,
-              frequency_penalty=0,
-              presence_penalty=0
+        with st.spinner("Generating Writeup ...."):
+            response = openai.ChatCompletion.create(
+              model="gpt-3.5-turbo",
+              messages=[{'role':'user','content':f'You act as reseracher. Write research review of more than 2000 words and include real references and in-line citations on topic \n\n{notes}\n\nDescription:'}]
+              #temperature=0.7,
+              #max_tokens=3000,
+              #top_p=1,
+              #frequency_penalty=0,
+              #presence_penalty=0
             )
         description = response['choices'][0]['text']
         st.subheader("Generated Writeup:")
