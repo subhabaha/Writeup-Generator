b49cf98488d15011ceca65814fe20efea76caada
subhabaha <51040380+subhabaha@users.noreply.github.com>
2023-03-23 08:18:20 +0530

    Update Literature_Review_Writer.py

--- Literature_Review_Writer.py
+++ Literature_Review_Writer.py
@@ -42,8 +42,8 @@ def main():
             )
             # Charge the payment
             charge = stripe.Charge.create(
-                amount=50,  # Charge INR 50
-                currency="inr",
+                amount=1,  # Charge INR 50
+                currency="usd",
                 description="Sample payment",
                 source=token.id,
             )
