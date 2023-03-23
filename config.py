c20febd09265c1d4402ce001f08109421c21c7d5
subhabaha <51040380+subhabaha@users.noreply.github.com>
2023-03-23 19:21:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,4 +3,5 @@ wordai_api_key = "82f416b96f4d5f5ef2eb504735281aa6"
 email = "subhabaha@msn.com"
 wordai_url = 'https://wai.wordai.com/api/avoid'
 stripe_api_key = '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' 
+wordai_url = 'https://wai.wordai.com/api/rewrite'
 
