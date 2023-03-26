4c14ecab1a9c9e2d1634f55a3c555e582b5824f4
subhabaha <51040380+subhabaha@users.noreply.github.com>
2023-03-26 15:39:25 +0530

    Update Literature_Review_Writer.py

--- Literature_Review_Writer.py
+++ Literature_Review_Writer.py
@@ -82,6 +82,8 @@ def main():
             stripe.PaymentIntent.confirm(
                 payment_intent.id,
                # payment_method=payment_method.id,
+                return_url = 'https://subhabaha-writeup-generator-literature-review-writer-0t4ldh.streamlit.app/',
+
             )
 
             # Display a success message
