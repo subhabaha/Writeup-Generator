49570bec41fcab3fe9cd4f0051d1d24887f7a40e
subhabaha <51040380+subhabaha@users.noreply.github.com>
2023-12-26 16:55:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_key = "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"
+api_key = "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"
 wordai_api_key = "82f416b96f4d5f5ef2eb504735281aa6"
 email = "subhabaha@msn.com"
 wordai_url = 'https://wai.wordai.com/api/avoid'
